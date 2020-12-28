b6930cb57c94e40c9fe8575434f618aac691e4c1
RyleeT <37136149+RyleeT@users.noreply.github.com>
2020-12-27 21:17:20 -0500

    Implement common colors in design-system

---
+++ src/design-system/common/colors.ts
@@ -0,0 +1,33 @@
+const colors = Object.freeze({
+  bgColor: '#282828',
+  light: '#cfcfcf',
+  dark: '#201F1E',
+  accent: '#4A8ECC',
+  accentLight: '#4FB1F4',
+  accentAlt: '#A66BE5',
+  text1: '#DEDEDE',
+  text2: '#999999',
+  text3: '#666666',
+  text4: '#333333',
+  textDisabled: '#494949',
+
+  alert: '#B91010',
+  warn: '#f89406',
+  success: '#4DB524',
+
+  surfaceLight1: '#FFFFFF',
+  surfaceLight2: '#F5F5F5',
+  surfaceLight3: '#EEEEEE',
+  surfaceDark1: '#030303',
+  surfaceDark2: '#111111',
+  surfaceDark3: '#222222',
+  surfaceDark4: '#333333',
+
+  chatText: '#838383',
+  chatDisabled: '#414141',
+
+  hbLight: '#849EB5',
+  hbDark: '#30394A',
+})
+
+export default colors
