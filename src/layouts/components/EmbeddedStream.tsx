b4e0c8f0ccfd017fbe4fe0a1b84f0e444fe63f36
Farhad Jay <effejaye@gmail.com>
2021-01-09 23:58:27 -0800

    Use key in stream iframe
    This ensures the iframe is re-rendered every time the embed details
    change so it doesn't affect browser history.

--- src/layouts/components/EmbeddedStream.tsx
+++ src/layouts/components/EmbeddedStream.tsx
@@ -5,7 +5,7 @@ import { embedUrlForEmbedDetails } from 'lib/embed-details'
 
 const EmbeddedStream = ({ style, embedDetails }) => {
    const embedUrl = embedUrlForEmbedDetails(embedDetails)
-   return <StyledEmbeddedStream style={style} src={embedUrl} />
+   return <StyledEmbeddedStream key={embedUrl} style={style} src={embedUrl} />
 }
 
 const StyledEmbeddedStream = styled.iframe`
