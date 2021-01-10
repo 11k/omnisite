f601c558e315661c251fc714973b96f28938e98b
Farhad Jay <effejaye@gmail.com>
2021-01-10 01:16:46 -0800

    Fix formatting

--- src/layouts/components/EmbeddedStream.tsx
+++ src/layouts/components/EmbeddedStream.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const EmbeddedStream = ({ style, embedUrl }) => {
-   return <StyledEmbeddedStream key={embedUrl} style={style} src={embedUrl} />
+  return <StyledEmbeddedStream key={embedUrl} style={style} src={embedUrl} />
 }
 
 const StyledEmbeddedStream = styled.iframe`
-   border: none;
+  border: none;
 `
 
 export default EmbeddedStream
--- src/lib/embed-details.ts
+++ src/lib/embed-details.ts
@@ -1,43 +1,43 @@
 export const embedUrlForEmbedDetails = (embedDetails) => {
-    const { platform, channel } = embedDetails
-    let params
-    switch (platform) {
-        case 'twitch':
-            params = new URLSearchParams({
-                channel: channel,
-                parent: process.env.NEXT_PUBLIC_TWITCH_EMBED_PARENT,
-            })
-            return 'https://player.twitch.tv?' + params.toString()
-        case 'youtube':
-            return 'https://www.youtube.com/embed/' + channel
-        case 'twitch-vod':
-            params = new URLSearchParams({
-                video: channel,
-                parent: process.env.NEXT_PUBLIC_TWITCH_EMBED_PARENT,
-            })
-            return 'https://player.twitch.tv?' + params.toString()
-        default:
-            return null
-    }
+  const { platform, channel } = embedDetails
+  let params
+  switch (platform) {
+    case 'twitch':
+      params = new URLSearchParams({
+        channel: channel,
+        parent: process.env.NEXT_PUBLIC_TWITCH_EMBED_PARENT,
+      })
+      return 'https://player.twitch.tv?' + params.toString()
+    case 'youtube':
+      return 'https://www.youtube.com/embed/' + channel
+    case 'twitch-vod':
+      params = new URLSearchParams({
+        video: channel,
+        parent: process.env.NEXT_PUBLIC_TWITCH_EMBED_PARENT,
+      })
+      return 'https://player.twitch.tv?' + params.toString()
+    default:
+      return null
+  }
 }
 
 export const embedDetailsFromHash = (hash) => {
-    if (!hash) {
-        return null
-    }
+  if (!hash) {
+    return null
+  }
 
-    hash = hash.substr(1)
-    if (!hash) {
-        return null
-    }
+  hash = hash.substr(1)
+  if (!hash) {
+    return null
+  }
 
-    const [platform, channel] = hash.split('/')
-    if (!platform || !channel) {
-        return null
-    }
+  const [platform, channel] = hash.split('/')
+  if (!platform || !channel) {
+    return null
+  }
 
-    return {
-        platform: platform,
-        channel: channel,
-    }
+  return {
+    platform: platform,
+    channel: channel,
+  }
 }
