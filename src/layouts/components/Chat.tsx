96608f9b9327e819d91e0021c8bfa0a29aa668d1
Farhad Jay <effejaye@gmail.com>
2021-01-10 19:55:45 -0800

    Add action bar above chat embed

--- src/layouts/components/Chat.tsx
+++ src/layouts/components/Chat.tsx
@@ -6,10 +6,32 @@ type Props = {
 }
 
 const Chat: React.FunctionComponent<Props> = ({ style }) => (
-  <StyledChat style={style} src="https://www.destiny.gg/embed/chat" />
+  <Container style={style}>
+    <ChatActionBar>
+      <div style={{ flex: 1 }}>
+        <button type="button">Refresh</button>
+      </div>
+      <button type="button">Swap</button>
+      <button type="button">Popout</button>
+      <button type="button">Close</button>
+    </ChatActionBar>
+    <EmbeddedChat src="https://www.destiny.gg/embed/chat" />
+  </Container>
 )
 
-const StyledChat = styled.iframe`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const ChatActionBar = styled.div`
+  display: flex;
+  padding: 4px;
+  height: 40px;
+`
+
+const EmbeddedChat = styled.iframe`
+  flex: 1;
   border: none;
 `
 
