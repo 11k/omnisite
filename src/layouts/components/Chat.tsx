dbd40b4fec1a1b0025f8a14116020aa91cab3a1f
Farhad Jay <effejaye@gmail.com>
2021-01-16 14:09:51 -0800

    Wire up button to refresh chat
    We need to pass a key into the chat iframe component to force a
    rerender, just like with the stream iframe.

--- src/layouts/components/Chat.tsx
+++ src/layouts/components/Chat.tsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import styled from 'styled-components'
 
 type Props = {
   style?: React.CSSProperties
 }
 
-const Chat: React.FunctionComponent<Props> = ({ style }) => (
-  <Container style={style}>
-    <ChatActionBar>
-      <div style={{ flex: 1 }}>
-        <button type="button">Refresh</button>
-      </div>
-      <button type="button">Swap</button>
-      <button type="button">Popout</button>
-      <button type="button">Close</button>
-    </ChatActionBar>
-    <EmbeddedChat src="https://www.destiny.gg/embed/chat" />
-  </Container>
-)
+const Chat: React.FunctionComponent<Props> = ({ style }) => {
+  // https://reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
+  const [chatRefreshKey, forceUpdate] = useReducer((x) => x + 1, 0)
+  const refreshChat = () => {
+    forceUpdate()
+  }
+
+  return (
+    <Container style={style}>
+      <ChatActionBar>
+        <div style={{ flex: 1 }}>
+          <button type="button" onClick={refreshChat}>
+            Refresh
+          </button>
+        </div>
+        <button type="button">Swap</button>
+        <button type="button">Popout</button>
+        <button type="button">Close</button>
+      </ChatActionBar>
+      <EmbeddedChat
+        key={chatRefreshKey}
+        src="https://www.destiny.gg/embed/chat"
+      />
+    </Container>
+  )
+}
 
 const Container = styled.div`
   display: flex;
