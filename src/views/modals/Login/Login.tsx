7226869775c369ea45b09bbf1f845daac93346d1
RyleeT <37136149+RyleeT@users.noreply.github.com>
2021-01-29 21:45:52 -0500

    Greatly improve login modal

--- src/views/modals/Login/Login.tsx
+++ src/views/modals/Login/Login.tsx
@@ -2,8 +2,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { signIn } from 'next-auth/client'
-import { Button, Modal } from 'react-bootstrap'
-import { Discord, Google } from '@styled-icons/fa-brands'
+import { Button, Col, Modal, Row } from 'react-bootstrap'
+import {
+  Discord,
+  Google,
+  Reddit,
+  Twitch,
+  Twitter,
+} from '@styled-icons/fa-brands'
 import styled, { css } from 'styled-components'
 
 import { colors } from 'design-system'
@@ -18,50 +24,113 @@ const Login: React.FC = () => {
   const handleClose = () => dispatch(toggleModal(ModalTypes.NONE))
 
   return (
-    <Modal centered onHide={handleClose} show={isVisible}>
-      {/* <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header> */}
+    <StyledModal centered onHide={handleClose} show={isVisible}>
       <Body>
-        <Button onClick={() => signIn('discord')}>
-          <StyledDiscord />
-          Discord
-        </Button>
-        <Button onClick={() => signIn('google')}>
-          <StyledGoogle />
-          Google
-        </Button>
+        <Row>
+          <StyledCol md={5} style={{ display: 'flex', paddingRight: 0 }}>
+            <FlexRow>
+              <FlexCol>
+                <Header>Login</Header>
+                <Text>
+                  Use your favorite provider to login and chat, subscribe, and
+                  more!
+                </Text>
+              </FlexCol>
+              <Divider />
+            </FlexRow>
+          </StyledCol>
+          <StyledCol md={7}>
+            <StyledButton
+              color={colors.twitch}
+              onClick={() => signIn('twitch')}
+            >
+              <StyledTwitch />
+              Twitch
+            </StyledButton>
+            <StyledButton
+              color={colors.google}
+              onClick={() => signIn('google')}
+            >
+              <StyledGoogle />
+              Google
+            </StyledButton>
+            <StyledButton
+              color={colors.twitter}
+              disabled
+              onClick={() => signIn('twitter')}
+            >
+              <StyledTwitter />
+              Twitter
+            </StyledButton>
+            <StyledButton
+              color={colors.reddit}
+              onClick={() => signIn('reddit')}
+            >
+              <StyledReddit />
+              Reddit
+            </StyledButton>
+            <StyledButton
+              color={colors.discord}
+              onClick={() => signIn('discord')}
+            >
+              <StyledDiscord />
+              Discord
+            </StyledButton>
+          </StyledCol>
+        </Row>
       </Body>
-      {/* <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer> */}
-    </Modal>
+    </StyledModal>
   )
 }
 
+const StyledModal = styled(Modal)`
+  .modal-content {
+    border: none !important;
+  }
+`
+
 const Body = styled(Modal.Body)`
-  /* background-color: ${colors.bgColor}; */
-  color: ${colors.text1};
-  /* border-radius: 5px; */
-  /* box-shadow: 0 1px 4px 1px rgba(0, 0, 0, 0.5); */
-  /* position: relative;
+  background-color: ${colors.bgColor};
+`
+
+const Divider = styled.div`
+  border-right: 1px solid ${colors.text1};
+  flex: 1;
+  margin: 1em 0px;
+  width: 2px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`
+
+const StyledCol = styled(Col)`
+  padding: 16px 32px;
+`
+
+// TODO: Create Flex and Text components in design-system
+
+const FlexCol = styled.div`
   display: flex;
-  flex-direction: column; */
-  /* width: 100%; */
-  /* background-clip: padding-box; */
-  /* border: 1px solid rgba(0, 0, 0, 0.2); */
-  /* border-radius: 0.3rem;
-  outline: 0; */
+  flex-direction: column;
+  justify-content: center;
+  padding-right: 32px;
+`
+const FlexRow = styled.div`
+  display: flex;
+`
+
+const Header = styled.h1`
+  color: white;
+`
+
+const Text = styled.p`
+  color: ${colors.text2};
 `
 
 const iconMixin = css`
   font-weight: 900;
-  width: 1.25em;
+  width: 1.5em;
   margin-right: 0.5em;
 `
 
@@ -71,6 +140,45 @@ const StyledDiscord = styled(Discord)`
 
 const StyledGoogle = styled(Google)`
   ${iconMixin}
+`
+
+const StyledReddit = styled(Reddit)`
+  ${iconMixin}
+`
+
+const StyledTwitch = styled(Twitch)`
+  ${iconMixin}
+`
+
+const StyledTwitter = styled(Twitter)`
+  ${iconMixin}
+`
+
+// TODO: Make this a design-system component
+const StyledButton = styled(Button)`
+  background-color: ${({ color }) => color};
+  border-color: ${({ color }) => color};
+  box-shadow: none !important;
+  transition: all 0.1s ease-in-out !important;
+  width: 100%;
+  :not(:last-of-type) {
+    margin-bottom: 0.5em;
+  }
+
+  // TODO: Consider box-shadow or border for focus and active states
+
+  :active,
+  :focus,
+  :hover {
+    background-color: ${({ color }) => color} !important;
+    border-color: ${({ color }) => color} !important;
+  }
+  :active {
+    transform: scale(0.95);
+  }
+  :hover {
+    filter: brightness(1.1);
+  }
 `
 
 export default Login
